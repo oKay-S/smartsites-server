f2bb7d59b1cff00e315f3300e7dc4baec4e3a0e7
KieranSoutter <72031734+KieranSoutter@users.noreply.github.com>
2022-01-26 21:46:41 +0000

    Update 0.5 (beta) Added: -about.html with respective redirection via server.js -contact.html with respective redirection via server.js -feed.html with respective redirection via server.js -review.html with respective redirection via server.js -added secondary css file for content pages -literature review pdf Updated: -index.html -server.js -style.css

--- server.js
+++ server.js
@@ -9,8 +9,27 @@ app.use(express.static(__dirname + '/public'));
 var port = PORT;
 // send our index.html file to the user for the home page
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public' , 'about.html'));
+});
+
+app.get('/feed', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public' , 'feed.html'));
+});
+
+app.get('/contact', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public' , 'contact.html'));
+});
+
+app.get('/review', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public' , 'review.html'));
+});
+
+
+
 // start the server
 app.listen(PORT);
 console.log('Express Server running at port:'.PORT);
